5f58e88993d4c1d559d04d622d12a0f231c9897a
cristian reyes <cristian.a.reyes@googlemail.com>
2014-05-01 22:47:04 -0400

    ejemplo hola mundo

--- base/urls.py
+++ base/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^index.html', 'hola.views.hola',name='index'),
     # url(r'^$', 'base.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
--- hola/views.py
+++ hola/views.py
@@ -1,3 +1,8 @@
+# coding - utf-8
+# Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hola(request):
+	return render('index.html',{'saludar':'hola mundo'}),
+
